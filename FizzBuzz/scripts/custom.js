83aa4a293a120fab8ea746ece23f142eb4d99011
Eli Jones <joneseli7395@gmail.com>
2020-10-07 21:22:24 -0400

    Initial commit

---
+++ FizzBuzz/scripts/custom.js
@@ -0,0 +1,61 @@
+﻿//FizzBuzz
+//Wire event handler for click of button
+document.getElementById("btnFizz").addEventListener('click', function() {
+
+    //Get user input
+    let f = Number(document.getElementById("userIn1").value); //User inputs
+    let b = Number(document.getElementById("userIn2").value);
+    FizzBuzz(f, b);
+
+    //Modulus operator 
+
+    //Run user input through named function
+    //let output = FizzBuzz(f, b);
+
+
+    //Output the results of FizzBuzz function
+    //document.getElementById("fizzy").innerHTML = output;
+
+})
+
+//Named function
+function FizzBuzz(f, b) {
+    let output = "";
+    let classCollect = document.getElementsByClassName("fizzy")
+
+    for (let loop = 1; loop < classCollect.length; loop++) {
+        let fizzRemainder = loop % f;
+        let buzzRemainder = loop % b;
+        //output += ``;
+
+        if (fizzRemainder === 0 && buzzRemainder === 0) {
+            //output += "FizzBuzz";
+            classCollect[loop].innerHTML = "FizzBuzz";
+            classCollect[loop].style.fontWeight = "bold";
+            //classCollect[loop].style.color = 
+
+
+        }
+        else if (fizzRemainder === 0) {
+            //output += "Fizz";
+            classCollect[loop].innerHTML = "Fizz";
+            classCollect[loop].style.fontWeight = "bold";
+            //classCollect[loop].style.color = 
+
+        }  
+        else if (buzzRemainder === 0) {
+            //output += "Buzz";
+            classCollect[loop].innerHTML = "Buzz";
+            classCollect[loop].style.fontWeight = "bold";
+            //classCollect[loop].style.color = 
+
+        } 
+        else {
+            //output += loop;
+            classCollect[loop].innerHTML = loop;
+        }
+    }
+        //return output;
+
+}
+
